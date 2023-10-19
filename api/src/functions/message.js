ff6f29b5f0f45fddb88e8a7b429dda4006a183ee
hakeem0114 <hakeem0114@gmail.com>
2023-10-19 12:41:11 -0400

    add API

---
+++ api/src/functions/message.js
@@ -0,0 +1,20 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        console.log('Tests')
+        const name = request.query.get('name') || await request.text() || 'Does not exist';
+
+        return { body: `Hello, Tumii Name: ${name}!` };
+    }
+});
+
+// module.exports = async function (context, req) {
+//     context.res.json({
+//         text: "Hello from the API"
+//     });
+// };
